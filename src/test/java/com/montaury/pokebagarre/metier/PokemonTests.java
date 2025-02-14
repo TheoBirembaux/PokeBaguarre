f3a0b42a9e2b049c711368e8b5df791ec1d5f010
Theo Birembaux <tbirembaux@iutbayonne.univ-pau.fr>
2025-02-14 12:06:04 +0100

    Initialisation test

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTests.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTests.java
@@ -1,7 +1,47 @@
+/*
+Classe de tests permettant de vérifier la cohérence de l'application PokeBaguarre
+Pokemon 1 > Pokemon 2 en attaque (Pokemon 1 gagne)
+Pokemon 2 > Pokemon 1 en attaque (Pokemon 2 gagne)
+Pokemon 1 = Pokemon 2 en attaque mais Pokemon 1 > Pokemon 2 en défense (gagnant Pokemon 1)
+Pokemon 1 = Pokemon 2 en attaque mais Pokemon 2 > Pokemon 1 en défense (gagnant Pokemon 2)
+Pokemon 1 = Pokemon 2 en attaque et Pokemon 1 = Pokemon 2 en défense (Pokemon 1 gagne)
+Pokemon 2 = Pokemon 1 en attaque et Pokemon 2 = Pokemon 1 en défense (Pokemon 2 gagne)
+* */
 package com.montaury.pokebagarre.metier;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PokemonTests {
 
+    @Test
+    void pokemon1_doit_gagner_avec_attq()
+    {
+
+    }
+
+    @Test
+    void pokemon2_doit_gagner_avec_attq()
+    {
+
+    }
+
+    @Test
+    void pokemon1_doit_gagner_avec_def()
+    {
+
+    }
+
+    @Test
+    void pokemon2_doit_gagner_avec_def()
+    {
+
+    }
+
+    @Test
+    void pokemon1_doit_gagner_egalite()
+    {
+
+    }
 }
